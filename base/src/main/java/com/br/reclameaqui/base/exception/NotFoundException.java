2deabc5625eeb952878f3e525c4f39a9e9f5fdb8
Filipe Borges <filipborgs48@gmail.com>
2021-06-06 09:20:17 -0300

    Creating treated exceptions

---
+++ base/src/main/java/com/br/reclameaqui/base/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.br.reclameaqui.base.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends TreatedExcetpion {
+
+    public NotFoundException(String desc) {
+        super(StringUtils.capitalize(desc) + " não encontrado");
+    }
+
+}
---
+++ base/src/main/java/com/br/reclameaqui/base/exception/TreatedExcetpion.java
@@ -0,0 +1,12 @@
+package com.br.reclameaqui.base.exception;
+
+public class TreatedExcetpion extends Exception {
+
+    public TreatedExcetpion(String message) {
+        super(message);
+    }
+
+    public TreatedExcetpion() {
+    }
+
+}
